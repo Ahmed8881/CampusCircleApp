e28f9dd31adefb0beac93da9eb97ffbbec728d72
Abdul Rehman <abdulrehmansafdar2928@gmail.com>
2026-07-05 22:54:01 +0500

    add release signing config and keystore to gitignore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,9 +17,19 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("release-key.jks")
+            storePassword = "campuscircle123"
+            keyAlias = "campuscircle"
+            keyPassword = "campuscircle123"
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
